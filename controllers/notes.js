ab496fa5c823e70d6c346424f4ed97d52b261076
David López Albajara <dlalbajara@gmail.com>
2021-02-24 13:12:46 +0100

    Refactor: async/await

--- controllers/notes.js
+++ controllers/notes.js
@@ -25,39 +25,41 @@ router
     }
   })
 
-router.get('/:id', (request, response, next) => {
-  Note.findById(request.params.id)
-    .then(note => {
+router
+  .route('/:id')
+  .get(async (request, response, next) => {
+    try {
+      const note = await Note.findById(request.params.id)
       if (note) {
         response.json(note)
       } else {
         response.status(404).end()
       }
-    })
-    .catch(error => next(error))
-})
-
-router.delete('/:id', (request, response, next) => {
-  Note.findByIdAndRemove(request.params.id)
-    .then(() => {
+    } catch (error) {
+      next(error)
+    }
+  })
+  .delete(async (request, response, next) => {
+    try {
+      await Note.findByIdAndRemove(request.params.id)
       response.status(204).end()
-    })
-    .catch(error => next(error))
-})
+    } catch (error) {
+      next(error)
+    }
+  })
+  .put((request, response, next) => {
+    const body = request.body
 
-router.put('/:id', (request, response, next) => {
-  const body = request.body
+    const note = {
+      content: body.content,
+      important: body.important
+    }
 
-  const note = {
-    content: body.content,
-    important: body.important
-  }
-
-  Note.findByIdAndUpdate(request.params.id, note, { new: true })
-    .then(updatedNote => {
-      response.json(updatedNote)
-    })
-    .catch(error => next(error))
-})
+    Note.findByIdAndUpdate(request.params.id, note, { new: true })
+      .then(updatedNote => {
+        response.json(updatedNote)
+      })
+      .catch(error => next(error))
+  })
 
 module.exports = router
