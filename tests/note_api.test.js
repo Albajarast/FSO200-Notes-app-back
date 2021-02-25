5941bac91b50bfd38f5442fed7f210307bd4bd31
David López Albajara <dlalbajara@gmail.com>
2021-02-25 11:17:38 +0100

    Fix: save notes in a loop

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -12,10 +12,11 @@ const api = supertest(app)
 
 beforeEach(async () => {
   await Note.deleteMany({})
-  let noteObject = new Note(initialNotes[0])
-  await noteObject.save()
-  noteObject = new Note(initialNotes[1])
-  await noteObject.save()
+
+  for (let note of initialNotes) {
+    let noteObject = new Note(note)
+    await noteObject.save()
+  }
 })
 
 test('notes are returned as json', async () => {
